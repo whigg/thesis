4b9613b071d8d4ef88f93f84c3a29d166e9fd6a1
whiteking64 <whiteking64@gmail.com>
2017-12-17 19:20:14 +0900

    update 2017/12/17 @Home

---
+++ miscellaneous/visual_3/basic_file.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+#iMac用
+
+latlon145_file_name = "../data/" + "latlon_ex.csv"
+latlon900_file_name = "../data/IC0_csv/" + "latlon_info.csv"
+grid900to145_file_name = "../data/" + "grid900to145.csv"
+ocean_grid_file = "../data/ocean_grid_145.csv"
+ocean_grid_145 = pd.read_csv(ocean_grid_file, header=None)
+ocean_idx = np.array(ocean_grid_145[ocean_grid_145==1].dropna().index)
+
+
+
+#macbook pro用
+"""
+latlon145_file_name = "../../data/" + "latlon_ex.csv"
+latlon900_file_name = "../../data/" + "latlon_info.csv"
+grid900to145_file_name = "../../data/" + "grid900to145.csv"
+ocean_grid_file = "../../data/ocean_grid_145.csv"
+ocean_grid_145 = pd.read_csv(ocean_grid_file, header=None)
+ocean_idx = np.array(ocean_grid_145[ocean_grid_145==1].dropna().index)
+"""
